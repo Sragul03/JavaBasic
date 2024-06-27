d0694449fec960e92bd3ee51ac389ca76059818f
ragul <ragul@LAPTOP-3BJ9QDCJ>
2024-06-27 11:02:54 +0530

    THIS IS MY FIRST PROJECT

---
+++ ADDITIONALPROGRAM/src/javabasic/day1.java
@@ -0,0 +1,11 @@
+package javabasic;
+
+public class day1 {
+	
+	public static void main(String[]args) {
+		System.out.println("hello");
+		System.out.println("5star");
+		System.out.print("do nothing");
+	}
+
+}
